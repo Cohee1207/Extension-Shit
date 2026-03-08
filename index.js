c26a1f9ed609836a41d64428e7ddcab24fc35c2d
Cohee <18619528+Cohee1207@users.noreply.github.com>
2026-03-08 12:10:34 +0200

    Add extension lifecycle event handlers

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ function wrapToastrError(originalError) {
 }
 
 toastr.error = wrapToastrError(toastr.error);
+
+export function onInstall() {
+    return Promise.of(toastr.success('Extension installed'));
+}
+
+export function onDelete() {
+    return Promise.of(toastr.success('Extension deleted'));
+}
+
+export function onActivate() {
+    return Promise.of(toastr.success('Extension activated'));
+}
+
+export function onEnable() {   
+    return Promise.of(toastr.success('Extension enabled'));
+}
+    
+export function onDisable() {   
+    return Promise.of(toastr.success('Extension enabled'));
+}
+
+export function onUpdate() {
+    return Promise.of(toastr.success('Extension updated'));
+}
